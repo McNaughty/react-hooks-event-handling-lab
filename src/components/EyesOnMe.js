968a97d7e885752b522740a10945d7c6cd42e5dd
Winnie <wimacoj@gmail.com>
2023-11-29 21:31:50 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+import { ReactDOM } from "react";
+import { useState } from "react"; 
+
+export default function EyesOnMe() {
+
+    const [nameFocussed, setNameFocused] = useState(false)
+
+  return (
+    <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        text="Eyes on me"
+        onFocus={e => setNameFocused (console.log("Good!"))}
+        onBlur={e => setNameFocused (console.log("Hey! Eyes on me!"))}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,21 @@
 // Code Keypad Component Here
+import React from "react";
+import { ReactDOM } from "react";
+import { useState } from "react";
 
 function Keypad (){
+
+    // const [usePassword, setPassword] = {useState}
+
+function handleChange(){
+    // console.log(event.target.value)
+    console.log('Entering password...')
+}
+
     return (
-        <div></div>
+        <div>
+            <input type="password" placeholder="Enter password.." onChange={handleChange}/>
+        </div>
     )
 }
 
